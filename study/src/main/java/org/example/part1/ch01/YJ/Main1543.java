1ea8f26bd684b8e7d90eb32f18fec61090f03510
yjin-jo <dpwls5100@icloud.com>
2024-05-26 16:46:33 +0900

    add : 1543

---
+++ study/src/main/java/org/example/part1/ch01/YJ/Main1543.java
@@ -0,0 +1,18 @@
+package org.example.part1.ch01.YJ;
+
+import java.util.Scanner;
+
+public class Main1543 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String word = sc.nextLine();
+        int i = 0;
+        while(str.contains(word)){// a a a
+            int idx = str.indexOf(word);// 1
+            i++;
+            str = str.substring(idx+word.length()); //a a
+        }
+        System.out.print(i);
+    }
+}
