0a75e5e765b910049b504a0b5d0bf15d556eb58e
Parkseojin <seojin5565@gmail.com>
2024-05-30 15:51:26 +0900

    add : 4949

---
+++ study/src/main/java/org/example/part2/ch02/Main4949.java
@@ -0,0 +1,56 @@
+package org.example.part2.ch02;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main4949 {
+
+  public static String isBalanced(String str) {
+
+    Stack<Character> stack = new Stack<>();
+
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      if (ch =='(') {
+        stack.push(ch);
+      } else if (ch == '['){
+        stack.push(ch);
+      } else if (ch == ')'){
+        if (stack.isEmpty()) {
+          return "no";
+        } else {
+          if (stack.peek() == '[') {
+            return "no";
+          }else {
+            stack.pop();
+          }
+        }
+
+      } else if (ch == ']'){
+        if (stack.isEmpty()) {
+          return "no";
+        } else {
+          if (stack.peek() == '(') {
+            return "no";
+          }else {
+            stack.pop();
+          }
+        }
+      }
+    }
+    if (stack.isEmpty()) {
+      return "yes";
+    }
+    return "no";
+  }
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String str = br.readLine();
+    while (!str.equals(".")) {
+      System.out.println(isBalanced(str.replace(" ", "")));
+      str = br.readLine();
+    }
+  }
+}
