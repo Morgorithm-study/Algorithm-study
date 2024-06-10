6a2266f1554709a84934ab396b67976e991192e3
Parkseojin <seojin5565@gmail.com>
2024-06-10 15:50:04 +0900

    add : 15654

--- study/src/main/java/org/example/part1/ch03/SJ/Main.java
+++ study/src/main/java/org/example/part1/ch03/SJ/Main.java
@@ -1,5 +1,32 @@
 package org.example.part1.ch03.SJ;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
 
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
+
+    int T = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < T; i++) {
+      int min = Integer.MAX_VALUE;
+      int sum = 0;
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < 7; j++) {
+        int n = Integer.parseInt(st.nextToken());
+        if (n % 2 == 0) {
+          sum += n;
+          min = Math.min(min, n);
+        }
+      }
+      sb.append(sum).append(" ").append(min);
+    }
+    System.out.println(sb);
+    br.close();
+  }
 }
